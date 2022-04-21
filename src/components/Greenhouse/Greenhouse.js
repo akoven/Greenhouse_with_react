0a3b859cf19da5897d12c3ecf0630014e00f152d
Attiya <adarensburg@live.com>
2022-04-21 14:44:40 -0500

    night/day toggle works, moving on to image changes

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,6 +1,7 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import {ThemeProvider, useTheme} from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
@@ -10,8 +11,8 @@ function Greenhouse() {
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+            src={nightImage}
+            alt='greenhouse'
       />
       <LightSwitch />
       <ClimateStats />
--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,18 @@
 import './LightSwitch.css';
+import {useTheme} from '../../context/ThemeContext';
+
+
+// console.log(useTheme);
 
 function LightSwitch() {
+  // const thing = useTheme()
+  // console.log(thing)
+  const {themeName, setThemeName} = useTheme();
+  console.log(themeName);
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div className="on" onClick={()=> setThemeName("day")}>DAY</div>
+      <div className="off" onClick={() => {setThemeName("night")}}>NIGHT</div>
     </div>
   );
 }
