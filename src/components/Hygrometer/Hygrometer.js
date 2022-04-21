9600c593da310c98bbc757520db826662137ff52
Attiya <adarensburg@live.com>
2022-04-21 17:07:38 -0500

    all done! :D

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -1,19 +1,33 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
 import { useClimate } from "../../context/ClimateContext";
+import {useState,useEffect} from 'react';
 
 
 function Hygrometer() {
-const { humidity } = useClimate();
+const { humidity, setHumidity } = useClimate();
+const [target, setTarget]=useState(humidity);
 
+useEffect(()=>{
+  const changeHumidity = setTimeout(() => {
+    if(target < humidity){
+      setTarget((prevHumidity) => prevHumidity+1)
+    }
+    if(target > humidity){
+      setTarget((prevHumidity) => prevHumidity-1)
+    }
+  },1000)
+  return () => clearTimeout(changeHumidity);
+
+},[humidity,target,setHumidity])
 
   return (
     <section>
       <h2>Hygrometer</h2>
       <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={10}
-        onAfterChange={(val) => {}}
+        value={target}
+        onAfterChange={(val) => {setHumidity(val)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
--- src/components/Thermometer/Thermometer.js
+++ src/components/Thermometer/Thermometer.js
@@ -1,9 +1,24 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
 import { useClimate } from "../../context/ClimateContext";
+import {useState, useEffect} from 'react';
 
 function Thermometer() {
   const { temperature, setTemperature } = useClimate();
+  const [target, setTarget]=useState(temperature);
+
+  useEffect(()=>{
+      const changeTemp = setTimeout(() => {
+        if(target > temperature){
+          setTemperature((prevTemp) => ++prevTemp)
+        }
+        if(target < temperature){
+          setTemperature((prevTemp) => --prevTemp)
+        }
+      },1000)
+      return () => clearTimeout(changeTemp);
+
+  },[temperature,target,setTemperature])
 
 
   return (
@@ -11,8 +26,8 @@ function Thermometer() {
       <h2>Thermometer</h2>
       <div className="actual-temp">Actual Temperature: {temperature}°F</div>
       <ReactSlider
-        value={temperature}
-        onAfterChange={(val) => {setTemperature(val)}}
+        value={target}
+        onAfterChange={(val) => {setTarget(val)}}
         className="thermometer-slider"
         thumbClassName="thermometer-thumb"
         trackClassName="thermometer-track"
