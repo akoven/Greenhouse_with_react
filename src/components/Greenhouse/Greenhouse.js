6bfb997d9bba7b3a0aa438031a882c2c8e198b33
Maica <maicasantos90@gmail.com>
2022-04-21 13:16:53 -0700

    switching themes works now

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,17 +1,19 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
-import {ThemeProvider, useTheme} from '../../context/ThemeContext';
+import {useTheme} from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
+
 function Greenhouse() {
+  const {themeName} = useTheme();
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={nightImage}
+            src={themeName === "day" ? dayImage : nightImage}
             alt='greenhouse'
       />
       <LightSwitch />
